f1e6ca4f8fbf653369590ed7d7410db013f0c583
Łukasz Opioła <lukasz.opiola@gmail.com>
2015-03-11 16:33:38 +0100

    Merge commit '088fdc20cb998aa7b3c77874094e2e5ea322475f' into feature/VFS-1046-systest-repo

--- docker/environment/docker.py
+++ docker/environment/docker.py
@@ -3,11 +3,12 @@ import os
 import sys
 import subprocess
 
-def run(image, docker_host=None, detach=False, dns_list=[], envs={}, hostname=None,
-        interactive=False, link={}, tty=False, rm=False, reflect=[],
-        volumes=[], name=None, workdir=None, user=None, run_params=[],
-        command=None):
 
+# noinspection PyDefaultArgument
+def run(image, docker_host=None, detach=False, dns_list=[], envs={},
+        hostname=None, interactive=False, link={}, tty=False, rm=False,
+        reflect=[], volumes=[], name=None, workdir=None, user=None,
+        run_params=[], command=None, stdin=None, stdout=None, stderr=None):
     cmd = ['docker']
 
     if docker_host:
@@ -69,9 +70,42 @@ def run(image, docker_host=None, detach=False, dns_list=[], envs={}, hostname=No
         cmd.extend(command)
 
     if detach:
-        return subprocess.check_output(cmd).decode('utf-8').strip()
+        return subprocess.check_output(cmd, stdin=stdin, stderr=stderr).decode(
+            'utf-8').strip()
 
-    return subprocess.call(cmd)
+    return subprocess.call(cmd, stdin=stdin, stderr=stderr, stdout=stdout)
+
+
+def exec_(container, command, docker_host=None, detach=False, interactive=False,
+          tty=False, output=False, stdin=None, stdout=None, stderr=None):
+    cmd = ['docker']
+
+    if docker_host:
+        cmd.extend(['-H', docker_host])
+
+    cmd.append('exec')
+
+    if detach:
+        cmd.append('-d')
+
+    if detach or sys.__stdin__.isatty():
+        if interactive:
+            cmd.append('-i')
+        if tty:
+            cmd.append('-t')
+
+    cmd.append(container)
+
+    if isinstance(command, str):
+        cmd.extend(['sh', '-c', command])
+    elif isinstance(command, list):
+        cmd.extend(command)
+
+    if detach or output:
+        return subprocess.check_output(cmd, stdin=stdin, stderr=stderr).decode(
+            'utf-8').strip()
+
+    return subprocess.call(cmd, stdin=stdin, stderr=stderr, stdout=stdout)
 
 
 def inspect(container, docker_host=None):
