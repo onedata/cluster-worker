67fca5390be720a8a55cb71e6afa32833bfc7f23
Michal Wrzeszcz <wrzeszcz@agh.edu.pl>
2016-03-01 21:11:43 +0100

    VFS-1670, Stress test

--- test_distributed/worker_stress_test_SUITE.erl
+++ test_distributed/worker_stress_test_SUITE.erl
@@ -43,8 +43,7 @@
     ]).
 
 -define(STRESS_NO_CLEARING_CASES, [
-        datastore_mixed_global_cache_test, datastore_mixed_db_test,
-        datastore_links_number_global_cache_test, datastore_links_number_db_test
+        datastore_mixed_global_cache_test, datastore_mixed_db_test
     ]).
 
 all() ->
