ae11e39dd28ebd408f751a8e955c740e365b103e
Michal Wrzeszcz <wrzeszcz@agh.edu.pl>
2016-03-13 11:45:34 +0100

    VFS-1739, test params

--- include/modules/datastore/datastore_common_internal.hrl
+++ include/modules/datastore/datastore_common_internal.hrl
@@ -60,9 +60,9 @@
 }).
 
 %% Max link map size in single links record
--define(LINKS_MAP_MAX_SIZE, 100).
+-define(LINKS_MAP_MAX_SIZE, 1000).
 %% Number of children owned by each link record
--define(LINKS_TREE_BASE, 2).
+-define(LINKS_TREE_BASE, 10).
 
 %% Internal semi-model used by document that holds links between documents
 -record(links, {
