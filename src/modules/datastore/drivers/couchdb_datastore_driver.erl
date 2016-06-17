ea0d51dd31391ce0752a539f55d8cc92066fc88a
Michal Wrzeszcz <wrzeszcz@agh.edu.pl>
2016-06-17 10:48:31 +0200

    VFS-2143, Upgrade seq num normalization

--- src/modules/datastore/drivers/couchdb_datastore_driver.erl
+++ src/modules/datastore/drivers/couchdb_datastore_driver.erl
@@ -507,6 +507,7 @@ normalize_seq(SeqBin) when is_binary(SeqBin) ->
     catch
         _:_ ->
             try
+                ?info("Changes loss: ~p", [SeqBin]),
                 [_SeqStable, SeqCurrent] = binary:split(SeqBin, <<"::">>),
                 normalize_seq(SeqCurrent)
             catch
