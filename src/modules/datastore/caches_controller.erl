01a1beb4baf0f0c8b2be48a16a6d0b51ce347f2e
Michal Wrzeszcz <wrzeszcz@agh.edu.pl>
2016-10-17 22:57:18 +0200

    VFS-2664 Update spec

--- src/modules/datastore/caches_controller.erl
+++ src/modules/datastore/caches_controller.erl
@@ -1202,7 +1202,7 @@ plan_next_throttling_check(_) ->
 %% Decreases interval if memory is growing too high.
 %% @end
 %%--------------------------------------------------------------------
--spec plan_next_throttling_check(MemoryChange :: float(), MemoryToStop :: float(), LastInterval :: integer()) ->
+-spec plan_next_throttling_check(MemoryChange :: number(), MemoryToStop :: float(), LastInterval :: integer()) ->
   non_neg_integer().
 plan_next_throttling_check(_MemoryChange, _MemoryToStop, 0) ->
   plan_next_throttling_check();
