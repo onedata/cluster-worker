aa4d10a4e86278d1c4c72113bd5108d44dad67c9
Michal Wrzeszcz <wrzeszcz@agh.edu.pl>
2016-12-07 18:31:15 +0100

    VFS-2854 Enable batch save

--- src/modules/datastore/drivers/couchdb_datastore_driver.erl
+++ src/modules/datastore/drivers/couchdb_datastore_driver.erl
@@ -203,28 +203,28 @@ save_link_doc(ModelConfig, Doc) ->
 %%--------------------------------------------------------------------
 -spec save_doc(model_behaviour:model_config(), datastore:document()) ->
     {ok, datastore:ext_key()} | datastore:generic_error().
-save_doc(#model_config{aggregate_db_writes = _Aggregate} = ModelConfig, ToSave = #document{}) ->
+save_doc(#model_config{aggregate_db_writes = Aggregate} = ModelConfig, ToSave = #document{}) ->
     {ok, {Pid, _}} = get_server(select_bucket(ModelConfig, ToSave)),
     Ref = make_ref(),
 
     % TODO - fix batch save performance
-%%    case Aggregate of
-%%        true ->
-%%            Pid ! {save_doc, {{self(), Ref}, {ModelConfig, ToSave}}},
-%%
-%%            receive
-%%                {Ref, Response} ->
-%%                    Response
-%%            after
-%%                ?DOCUMENT_AGGREGATE_SAVE_TIMEOUT ->
-%%                    {error, gateway_loop_timeout}
-%%            end;
-%%        false ->
+    case Aggregate of
+        true ->
+            Pid ! {save_doc, {{self(), Ref}, {ModelConfig, ToSave}}},
+
+            receive
+                {Ref, Response} ->
+                    Response
+            after
+                ?DOCUMENT_AGGREGATE_SAVE_TIMEOUT ->
+                    {error, gateway_loop_timeout}
+            end;
+        false ->
             [RawDoc] = make_raw_doc(ModelConfig, [ToSave]),
             {ok, [RawRes]} = db_run(select_bucket(ModelConfig, ToSave), couchbeam, save_docs,
                 [[RawDoc], ?DEFAULT_DB_REQUEST_TIMEOUT_OPT], 3),
-            parse_response(save, RawRes).
-%%    end.
+            parse_response(save, RawRes)
+    end.
 
 
 %%--------------------------------------------------------------------
