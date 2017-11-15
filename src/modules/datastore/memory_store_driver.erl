93e613b7c08e172073ce51ec13874538b6255574
Michal Wrzeszcz <wrzeszcz@agh.edu.pl>
2017-11-15 15:22:32 +0100

    VFS-3813 Update spec

--- src/modules/datastore/memory_store_driver.erl
+++ src/modules/datastore/memory_store_driver.erl
@@ -309,24 +309,6 @@ increment_rev(#{persistence := false}, Doc) ->
 increment_rev(Ctx, Doc) ->
   set_next_rev(Ctx, Doc).
 
-set_next_rev(#{no_rev := true}, Doc) ->
-  Doc;
-set_next_rev(_Ctx, #document{rev = []} = Doc) ->
-  Hash = datastore_utils2:gen_key(),
-  Rev = <<(integer_to_binary(1))/binary, "-", Hash/binary>>,
-  Doc#document{rev = [Rev]};
-set_next_rev(_Ctx, #document{rev = [Rev0 | _] = Revs} = Doc) ->
-  [Gen0, _] = binary:split(Rev0, <<"-">>),
-  Gen = binary_to_integer(Gen0),
-  Gen2 = Gen + 1,
-  Hash = datastore_utils2:gen_key(),
-  Rev = <<(integer_to_binary(Gen2))/binary, "-", Hash/binary>>,
-  Length = application:get_env(?CLUSTER_WORKER_APP_NAME,
-    couchbase_revision_history_length, 1),
-  Revs2 = lists:sublist([Rev | Revs], Length),
-
-  Doc#document{rev = Revs2}.
-
 %%--------------------------------------------------------------------
 %% @doc
 %% Converts given binary into tuple {revision_num, hash}.
@@ -341,6 +323,31 @@ rev_to_info(Rev) ->
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Creates and stores next revision hash in a document.
+%% @end
+%%--------------------------------------------------------------------
+-spec set_next_rev(couchbase_driver:ctx(), datastore:document()) ->
+  datastore:document().
+set_next_rev(#{no_rev := true}, Doc) ->
+  Doc;
+set_next_rev(_Ctx, #document{rev = []} = Doc) ->
+  Hash = datastore_utils2:gen_key(),
+  Rev = <<(integer_to_binary(1))/binary, "-", Hash/binary>>,
+  Doc#document{rev = [Rev]};
+set_next_rev(_Ctx, #document{rev = [Rev0 | _] = Revs0} = Doc) ->
+  [Gen0, _] = binary:split(Rev0, <<"-">>),
+  Gen = binary_to_integer(Gen0) + 1,
+  Hash = datastore_utils2:gen_key(),
+  Rev = <<(integer_to_binary(Gen))/binary, "-", Hash/binary>>,
+  Length = application:get_env(?CLUSTER_WORKER_APP_NAME,
+    couchbase_revision_history_length, 1),
+  Revs = lists:sublist([Rev | Revs0], Length),
+
+  Doc#document{rev = Revs}.
 
 %%--------------------------------------------------------------------
 %% @private
