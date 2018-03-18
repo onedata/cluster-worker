73e6d47f7e8e22a1cc9efe75ebb55cfe6ebbc75a
Michal Wrzeszcz <wrzeszcz@agh.edu.pl>
2018-03-18 22:28:36 +0100

    Update views creation, cberl and cberl test

--- src/modules/datastore/couchbase/couchbase_view.erl
+++ src/modules/datastore/couchbase/couchbase_view.erl
@@ -37,9 +37,17 @@ save_design_doc(Connection, DesignName, EJson) ->
     Path = <<"_design/", DesignName/binary>>,
     Body = jiffy:encode(EJson),
     ContentType = <<"application/json">>,
-    parse_design_doc_response(save,
+    Ans = parse_design_doc_response(save,
         cberl:http(Connection, view, put, Path, ContentType, Body, ?TIMEOUT)
-    ).
+    ),
+
+    case Ans of
+        ok ->
+            Views = get_views(EJson),
+            test_views(Connection, DesignName, Views, 5, 0);
+        _ ->
+            Ans
+    end.
 
 %%--------------------------------------------------------------------
 %% @doc
@@ -178,3 +186,42 @@ get_query_param({group, true}) -> <<"group=true">>;
 get_query_param({group, false}) -> <<"group=false">>;
 get_query_param({group_level, Level}) ->
     <<"group_level=", (integer_to_binary(Level))/binary>>.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Get views list from ejson.
+%% @end
+%%--------------------------------------------------------------------
+-spec get_views(term()) -> list().
+get_views([]) ->
+    [];
+get_views([Element | Tail]) ->
+    get_views(Element) ++ get_views(Tail);
+get_views({Element}) ->
+    get_views(Element);
+get_views({<<"views">>, {Views}}) ->
+    lists:map(fun({Name, _}) -> Name end, Views).
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Tests if all views can be queried.
+%% @end
+%%--------------------------------------------------------------------
+-spec test_views(cberl:connection(), couchbase_driver:design(),
+    list(), non_neg_integer(), non_neg_integer()) -> ok | {error, term()}.
+test_views(_Connection, _DesignName, [], _Repeats, _SleepTime) ->
+    ok;
+test_views(_Connection, _DesignName, _Views, 0, _SleepTime) ->
+    {error, cannot_query_views};
+test_views(Connection, DesignName, Views, Repeats, SleepTime) ->
+    timer:sleep(SleepTime),
+    Views2 = lists:foldl(fun(View, Acc) ->
+        case query(Connection, DesignName, View, [
+            {stale, false}, {key, <<"key">>}]) of
+            {ok, _} -> Acc;
+            _ -> [View | Acc]
+        end
+    end, [], Views),
+    test_views(Connection, DesignName, Views2, Repeats - 1, SleepTime * 2 + 100).
