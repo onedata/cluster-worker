be822f74ade587892e3776a8b7db9313dd0164f0
Michał Stanisz <mch.stanisz@gmail.com>
2024-01-15 17:17:10 +0100

    VFS-11574 Improvements according to code review

--- src/utils/safe_mode.erl
+++ src/utils/safe_mode.erl
@@ -7,60 +7,112 @@
 %%%-------------------------------------------------------------------
 %%% @doc
 %%% This module provides functions to manipulate safe mode in the cluster.
+%%% There are 3 possible states of safe mode:
+%%%     * awaiting_cluster_init - implicit state before cluster is ready and fully operational
+%%%       (it is possible to whitelist PIDs that are excluded from safe mode limitations);
+%%%     * disabled - safe mode is disabled;
+%%%     * enabled_manually - manually activated by cluster admin.
 %%% @end
 %%%-------------------------------------------------------------------
 -module(safe_mode).
 -author("Michal Stanisz").
 
--export([enable/0, disable/0, disable_if_not_set/0, is_enabled/0]).
--export([is_pid_allowed/1, whitelist_pid/1]).
+% API
+-export([enable_manually/0, disable_manually/0, report_node_initialized/0]).
+-export([should_enforce/0, should_enforce_for_pid/1, whitelist_pid/1]).
 
--define(SAVE_MODE_ENV, save_mode).
--define(SAVE_MODE_WHITELIST_CACHE, save_mode_whitelist_cache).
+% RPC API
+-export([enable_manually_on_single_node/0, disable_manually_on_single_node/0]).
+
+-define(SAFE_MODE_ENV, safe_mode).
+-define(SAFE_MODE_WHITELIST_CACHE, safe_mode_whitelist_cache).
+
+-define(AWAITING_CLUSTER_INIT_STATE, awaiting_cluster_init).
+-define(MANUALLY_ENABLED_STATE, manually_enabled).
+-define(DISABLED_STATE, disabled).
+
+-type state() :: ?AWAITING_CLUSTER_INIT_STATE | ?DISABLED_STATE | ?MANUALLY_ENABLED_STATE.
 
 %%%===================================================================
 %%% API
 %%%===================================================================
 
--spec enable() -> ok.
-enable() ->
-    cluster_worker:set_env(?SAVE_MODE_ENV, true).
+-spec enable_manually() -> ok | error.
+enable_manually() ->
+    rpc_all_nodes(enable_manually_on_single_node).
 
 
--spec disable() -> ok.
-disable() ->
-    node_cache:clear(?SAVE_MODE_WHITELIST_CACHE),
-    cluster_worker:set_env(?SAVE_MODE_ENV, false).
+-spec disable_manually() -> ok.
+disable_manually() ->
+    rpc_all_nodes(disable_manually_on_single_node).
 
 
--spec disable_if_not_set() -> ok.
-disable_if_not_set() ->
-    case cluster_worker:get_env(?SAVE_MODE_ENV, not_set) of
-        not_set ->
-            disable();
+-spec report_node_initialized() -> ok.
+report_node_initialized() ->
+    case get_state() of
+        ?AWAITING_CLUSTER_INIT_STATE ->
+            disable_manually_on_single_node();
         _ ->
             ok
     end.
 
 
--spec is_enabled() -> boolean().
-is_enabled() ->
-    cluster_worker:get_env(?SAVE_MODE_ENV, true).
+-spec should_enforce() -> boolean().
+should_enforce() ->
+    get_state() =/= ?DISABLED_STATE.
 
 
--spec is_pid_allowed(pid()) -> boolean().
-is_pid_allowed(Pid) ->
-    case is_enabled() of
+-spec should_enforce_for_pid(pid()) -> boolean().
+should_enforce_for_pid(Pid) ->
+    case should_enforce() of
         false ->
-            true;
+            false;
         true ->
-            lists:member(Pid, node_cache:get(?SAVE_MODE_WHITELIST_CACHE, []))
+            not lists:member(Pid, node_cache:get(?SAFE_MODE_WHITELIST_CACHE, []))
     end.
 
 
 -spec whitelist_pid(pid()) -> ok.
 whitelist_pid(Pid) ->
-    {ok, _} = node_cache:update(?SAVE_MODE_WHITELIST_CACHE, fun(PrevList) ->
+    {ok, _} = node_cache:update(?SAFE_MODE_WHITELIST_CACHE, fun(PrevList) ->
         {ok, [Pid | PrevList], infinity}
     end, [Pid]),
     ok.
+
+
+%%%===================================================================
+%%% RPC API
+%%%===================================================================
+
+-spec enable_manually_on_single_node() -> ok | error.
+enable_manually_on_single_node() ->
+    case get_state() of
+        ?AWAITING_CLUSTER_INIT_STATE ->
+            error;
+        _ ->
+            cluster_worker:set_env(?SAFE_MODE_ENV, ?MANUALLY_ENABLED_STATE)
+    end.
+
+
+-spec disable_manually_on_single_node() -> ok.
+disable_manually_on_single_node() ->
+    node_cache:clear(?SAFE_MODE_WHITELIST_CACHE),
+    cluster_worker:set_env(?SAFE_MODE_ENV, ?DISABLED_STATE).
+
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%% @private
+-spec get_state() -> state().
+get_state() ->
+    cluster_worker:get_env(?SAFE_MODE_ENV, ?AWAITING_CLUSTER_INIT_STATE).
+
+
+%% @private
+-spec rpc_all_nodes(atom()) -> ok.
+rpc_all_nodes(Function) ->
+    lists:foreach(fun(Node) ->
+        ok = rpc:call(Node, ?MODULE, Function, [])
+    end, consistent_hashing:get_all_nodes()).
